21a41a2447059b2f29be281284c505c0c396cc54
Erik Zemčík <zemcik.erik@gmail.com>
2019-10-24 08:39:12 +0200

    Created test

---
+++ Open-Lab-02.04/Farm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_02._04
+{
+    class Farm
+    {
+        public int GetLegsCount(int chickens, int cows, int pigs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Open-Lab-02.04/Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections;
+
+namespace Open_Lab_02._04
+{
+    [TestFixture]
+    class Tests
+    {
+
+        private Farm farm;
+
+        private const int RandMaxNum = 1000;
+        private const int RandSeed = 1000789;
+        private const int RandTestCasesCount = 98;
+
+        [OneTimeSetUp]
+        public void Init() => farm = new Farm();
+
+        [TestCase(5, 2, 3, 30)]
+        [TestCase(10, 12, 13, 120)]
+        [TestCaseSource(nameof(GetRandom))]
+        public void GetLegsCountTest(int chickens, int cows, int pigs, int expectedOutput) =>
+            Assert.AreEqual(expectedOutput, farm.GetLegsCount(chickens, cows, pigs));
+
+        private static IEnumerable GetRandom()
+        {
+            var random = new Random(RandSeed);
+
+            for (var i = 0; i < RandTestCasesCount; i++)
+            {
+                var chickens = random.Next(RandMaxNum);
+                var cows = random.Next(RandMaxNum);
+                var pigs = random.Next(RandMaxNum);
+
+                yield return new TestCaseData(chickens, cows, pigs, chickens * 2 + (cows + pigs) * 4);
+            }
+        }
+
+    }
+}
